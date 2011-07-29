dcd761a4a71cebdcf3953aadce56f76ec40c893b
Fil Maj <filip.maj@nitobi.com>
2011-07-29 15:05:50 -0700

    randomly flailing on why xhr response from build is failing

--- js/hydra.js
+++ js/hydra.js
@@ -48,6 +48,8 @@
       }
     }
 
+    xhr.setRequestHeader("Accept", "application/json");
+
     xhr.onreadystatechange = function(){
       if ( xhr.readyState == 4 ) {
         if ( xhr.status == 200 || xhr.status == 0) {
